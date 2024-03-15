5777410973e8312970e04b5c66a47eb81061b5b7
Markokakola <148535019+markokakola@users.noreply.github.com>
2024-03-15 17:28:59 +0500

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,23 +2,89 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using Microsoft.Maps.MapControl.WPF;
+using System.Windows.Media.Imaging;
 
 namespace WpfAppMapp
 {
     public partial class MainWindow : Window
     {
-        public string filePath = "C:\\Users\\Student13\\source\\repos\\WpfAppMapp\\WpfAppMapp\\pushpins.txt"; 
-        private List<Location> pushpinLocations = new List<Location>(); 
+        public string filePath = "pushpins.txt";
+        public string projectFolder = "C:\\Users\\Student13\\source\\repos\\WpfAppMapp\\WpfAppMapp\\";
+        private List<Location> pushpinLocations = new List<Location>();
 
         public MainWindow()
         {
             InitializeComponent();
             myMap.MouseDoubleClick += MyMap_MouseDoubleClick;
             btnRemovePushpins.Click += BtnRemovePushpins_Click;
-            Loaded += MainWindow_Loaded; 
+            Loaded += MainWindow_Loaded;
             Closing += MainWindow_Closing;
+        }
+
+        private void BtnInstructions_Click(object sender, RoutedEventArgs e)
+        {
+            Window instructionsWindow = new Window();
+            instructionsWindow.Title = "Руководство";
+            instructionsWindow.Width = 1150;
+            instructionsWindow.Height = 900;
+            instructionsWindow.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            StackPanel stackPanel = new StackPanel();
+            stackPanel.Margin = new Thickness(10);
+
+            TextBlock textBlock1 = new TextBlock();
+            textBlock1.Text = "Руководство:\n\n1. Для установки метки, нажмите на указанную вами точку два раза. ";
+            textBlock1.TextWrapping = TextWrapping.Wrap;
+            textBlock1.FontSize = 20; 
+            textBlock1.FontWeight = FontWeights.Bold; 
+            stackPanel.Children.Add(textBlock1);
+
+            Image image1 = new Image();
+            image1.Source = new BitmapImage(new Uri(projectFolder+"screen1.png"));
+            image1.Margin = new Thickness(0, 10, 0, 0); 
+            stackPanel.Children.Add(image1);
+
+            TextBlock textBlock2 = new TextBlock();
+            textBlock2.Text = "2. Нажмите 'Удалить метки' чтобы убрать все установленные метки. ";
+            textBlock2.TextWrapping = TextWrapping.Wrap;
+            textBlock2.FontSize = 16; 
+            textBlock2.FontWeight = FontWeights.Bold; 
+            stackPanel.Children.Add(textBlock2);
+
+            Image image2 = new Image();
+            image2.Source = new BitmapImage(new Uri(projectFolder+"screen25.png"));
+            image2.Margin = new Thickness(0, 10, 0, 0); 
+            stackPanel.Children.Add(image2);
+
+            TextBlock textBlock3 = new TextBlock();
+            textBlock3.Text = "3. Метки сохраняют свои позиции при закрытии приложения. ";
+            textBlock3.TextWrapping = TextWrapping.Wrap;
+            textBlock3.FontSize = 16; 
+            textBlock3.FontWeight = FontWeights.Bold; 
+            stackPanel.Children.Add(textBlock3);
+
+            Image image3 = new Image();
+            image3.Source = new BitmapImage(new Uri(projectFolder+"screen3.png"));
+            image3.Margin = new Thickness(0, 10, 0, 0); 
+            stackPanel.Children.Add(image3);
+
+            Image image4 = new Image();
+            image4.Source = new BitmapImage(new Uri(projectFolder+"screen4.png"));
+            image4.Margin = new Thickness(0, 10, 0, 0); 
+            stackPanel.Children.Add(image4);
+
+            // Create a scroll viewer and set its content to the stack panel
+            ScrollViewer scrollViewer = new ScrollViewer();
+            scrollViewer.VerticalScrollBarVisibility = ScrollBarVisibility.Auto; 
+            scrollViewer.Content = stackPanel;
+
+            // Set the scroll viewer as the content of the window
+            instructionsWindow.Content = scrollViewer;
+
+            instructionsWindow.ShowDialog();
         }
 
         private void MyMap_MouseDoubleClick(object sender, MouseButtonEventArgs e)
@@ -41,13 +107,11 @@ namespace WpfAppMapp
 
         private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            // Сохраняем метки при закрытии окна приложения
             SavePushpinLocationsToFile();
         }
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            // Загружаем метки при запуске приложения
             LoadPushpinLocationsFromFile();
         }
 
@@ -57,19 +121,14 @@ namespace WpfAppMapp
             {
                 if (File.Exists(filePath))
                 {
-                    // Clear existing pushpins
                     myMap.Children.Clear();
 
-                    // Read all lines from the file
                     string[] lines = File.ReadAllLines(filePath);
 
-                    // Iterate through each line
                     foreach (string line in lines)
                     {
-                        // Split the line by comma to get latitude and longitude
                         string[] parts = line.Split(';');
 
-                        // Ensure that the line has valid latitude and longitude
                         if (parts.Length == 2 && double.TryParse(parts[0], out double latitude) && double.TryParse(parts[1], out double longitude))
                         {
                             Location location = new(latitude, longitude);
@@ -81,7 +140,7 @@ namespace WpfAppMapp
                         }
                     }
 
-                    MessageBox.Show($"Loaded {pushpinLocations.Count} pushpin locations from file.");
+                    MessageBox.Show($"Загружено {pushpinLocations.Count} меток.");
                 }
                 else
                 {
@@ -108,7 +167,7 @@ namespace WpfAppMapp
                     }
                 }
 
-                MessageBox.Show($"Saved {pushpinLocations.Count} pushpin locations to file.");
+                MessageBox.Show($"Сохранено {pushpinLocations.Count} меток.");
             }
             catch (Exception ex)
             {
